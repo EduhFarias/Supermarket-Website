60b2f0f3264e932bb31b45e838d0de7caa160a8f
Eduardo Henrique <ehfs@ic.ufal.br>
2022-05-26 01:46:49 -0300

    Add fab to profile page

---
+++ src/shared/user.js
@@ -0,0 +1,11 @@
+function editProfile() {
+  document.getElementById('main-div').innerHTML = 'edit profile';
+}
+
+function paymentMethods() {
+  document.getElementById('main-div').innerHTML = 'payment methods';
+}
+
+function showHistoric() {
+  document.getElementById('main-div').innerHTML = 'historic';
+}
